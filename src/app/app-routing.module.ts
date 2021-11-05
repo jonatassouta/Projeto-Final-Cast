acda1116a4ce41962b0065d183652d60d1852a08
jonatassouta <jonatas.souta81@gmail.com>
2021-11-05 13:47:39 -0300

    Add Form de Contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HomeComponent } from './home/home.component';
+import { ContateNosComponent } from './contate-nos/contate-nos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contate-nos', component: ContateNosComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { ContateNosComponent } from './contate-nos/contate-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
+    ContateNosComponent,
   ],
   imports: [
     BrowserModule,
